b0b477afb0ec1c3e6f4e3690d0917db3c2fc5d58
Sanjay3511 <sanjay@electricpe.com>
2023-04-04 18:37:28 +0530

    rotation ,app activity change in utils

--- src/test/java/apiDemoApp/AppRotation.java
+++ src/test/java/apiDemoApp/AppRotation.java
@@ -1,18 +1,19 @@
 package apiDemoApp;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.Activity;
 import org.baseconfig.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
-public class PreferenceTest extends BaseTest {
-    @Test
-    public void accessibility() {
+public class AppRotation extends BaseTest {
+    public void rotationTest() {
+
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
         driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
         driver.findElement(By.id("android:id/checkbox")).click();
+        DeviceRotation landScape = new DeviceRotation(0,0,90);
+        driver.rotate(landScape);
         driver.findElements(AppiumBy.className("android.widget.RelativeLayout")).get(1).click();
         String actual = driver.findElement(By.id("android:id/alertTitle")).getText();
         Assert.assertEquals(actual,"WiFi settings");
--- src/test/java/org/baseconfig/BaseTest.java
+++ src/test/java/org/baseconfig/BaseTest.java
@@ -3,7 +3,10 @@ package org.baseconfig;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.DeviceRotation;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,19 +15,20 @@ import org.testng.annotations.*;
 import resources.AppPath;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class BaseTest {
    public static AppiumDriver driver;
    public DesiredCapabilities caps;
-   public String appPath = new AppPath().getApiDemoDebug();
+   public String appPath = new AppPath().getMewtApk();
    @BeforeClass
     public void setUp() throws MalformedURLException {
-       caps = new DesiredCapabilities();
        caps.setCapability("platFormName","Android");
        caps.setCapability("automationName","UIAutomator2");
        caps.setCapability("deviceName","emulator-5554");
        caps.setCapability("app",appPath);
        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
    }
    @AfterClass
    public void tearDown() {
